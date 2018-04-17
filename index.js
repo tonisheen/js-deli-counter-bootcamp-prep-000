f2ab8b18f130900f8091357a9bded525389cc0a3
Newton Migosi <newtonmigosi@gmail.com>
2018-04-17 12:16:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(deliLine) {
   if (deliLine.length === 0) {
     return 'There is nobody waiting to be served!'; 
   } else {
-    return deliline.shift();
+    return deliLine.shift();
   }
 }
 
